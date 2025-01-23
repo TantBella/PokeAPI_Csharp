7f8fd30b9607d3bee691013b8db5388acbf405ba
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-23 11:34:55 +0100

    CRUD

--- PokemonAPI/PokemonAPI/Program.cs
+++ PokemonAPI/PokemonAPI/Program.cs
@@ -34,11 +34,54 @@ namespace PokemonAPI
                 new Pokemon {Id=4, Name = "Charmander", Type="Fire"}
             };
 
+            app.MapPost("/pokemon", (Pokemon pokemon) =>
+            {
+                pokemons.Add(pokemon);
+                return Results.Ok(pokemon);
+            });
+
             app.MapGet("/pokemons", () =>
             {
                 return Results.Ok(pokemons);
             });
-                     
+
+            app.MapGet("/pokemon/{id}", (int id) =>
+            {
+                var pokemon = pokemons.Find(p => p.Id == id);
+
+                if (pokemon == null) {
+                    return Results.NotFound("Pokemon with this id not found"); }
+                return Results.Ok(pokemon);
+            });
+
+            app.MapPut("/pokemon/{id}", (Pokemon updatePokemon, int id) => {         
+            var pokemon = pokemons.Find(p => p.Id == id);
+            if(pokemon == null)
+            {
+                return Results.NotFound("Pokemon with this id not found");
+            }
+                //pokemon.Id = updatePokemon.Id;
+                //pokemon.Name = updatePokemon.Name;
+                //pokemon.Type = updatePokemon.Type;
+
+                pokemons[id-1] = updatePokemon;
+
+                return Results.Ok(pokemon);
+             });
+
+            app.MapDelete("/pokemon/{id}", (int id) =>
+            {
+                var pokemonToRemove = pokemons.Find(p => p.Id == id);
+                if (pokemonToRemove == null)
+                {
+                    return Results.NotFound("Pokemon with this id not found");
+                }
+
+                pokemons.Remove(pokemonToRemove);
+                return Results.Ok("Pokemon was removed successfully");
+            });
+
+
             app.Run();
         }
     }
